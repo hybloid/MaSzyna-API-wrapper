ad9353313954f6724d8b52010a38af9882be880a
DoS <karoltomaszewskimusic@gmail.com>
2024-12-03 16:30:52 +0100

    Untracked files?

---
+++ CMakeFiles/maszyna_physics_server.dir/godot-cpp/gen/src/classes/a_star2d.cpp.o.d
@@ -0,0 +1,269 @@
+CMakeFiles/maszyna_physics_server.dir/godot-cpp/gen/src/classes/a_star2d.cpp.o: \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/src/classes/a_star2d.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/classes/a_star2d.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/classes/ref.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/defs.hpp \
+ /usr/include/c++/11/cstddef \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/cpu_defines.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/include/c++/11/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/c++/11/cstring /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/classes/object.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/classes/global_constants.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/classes/wrapped.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/memory.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/error_macros.hpp \
+ /usr/include/c++/11/atomic /usr/include/c++/11/bits/atomic_base.h \
+ /usr/include/c++/11/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/11/bits/move.h /usr/include/c++/11/type_traits \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/godot.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gdextension/gdextension_interface.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/property_info.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/variant.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/builtin_types.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/aabb.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/plane.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/vector3.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/math.hpp \
+ /usr/include/c++/11/cmath /usr/include/c++/11/bits/cpp_type_traits.h \
+ /usr/include/c++/11/ext/type_traits.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+ /usr/include/c++/11/bits/std_abs.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/array_helpers.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/basis.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/quaternion.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/callable.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/callable_custom.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/object_id.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/string_name.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/string.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/char_string.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/templates/cowdata.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/templates/safe_refcount.hpp \
+ /usr/include/c++/11/new /usr/include/c++/11/bits/exception.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/char_utils.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/color.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/dictionary.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/node_path.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_byte_array.hpp \
+ /usr/include/c++/11/initializer_list \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_color_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_float32_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_float64_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_int32_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_int64_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_string_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_vector2_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/vector2.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_vector3_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/packed_vector4_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/vector4.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/projection.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/rect2.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/rect2i.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/vector2i.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/rid.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/signal.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/transform2d.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/transform3d.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/vector3i.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/vector4i.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/variant_size.hpp \
+ /usr/include/c++/11/array /usr/include/c++/11/utility \
+ /usr/include/c++/11/bits/stl_relops.h \
+ /usr/include/c++/11/bits/stl_pair.h \
+ /usr/include/c++/11/bits/functexcept.h \
+ /usr/include/c++/11/bits/exception_defines.h \
+ /usr/include/c++/11/bits/stl_algobase.h \
+ /usr/include/c++/11/ext/numeric_traits.h \
+ /usr/include/c++/11/bits/stl_iterator_base_types.h \
+ /usr/include/c++/11/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/11/bits/concept_check.h \
+ /usr/include/c++/11/debug/assertions.h \
+ /usr/include/c++/11/bits/stl_iterator.h \
+ /usr/include/c++/11/bits/ptr_traits.h /usr/include/c++/11/debug/debug.h \
+ /usr/include/c++/11/bits/predefined_ops.h \
+ /usr/include/c++/11/bits/range_access.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/builtin_vararg_methods.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/templates/list.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/templates/sort_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/templates/vector.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/templates/search_array.hpp \
+ /usr/include/c++/11/climits \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/uio_lim.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/typed_array.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/classes/ref_counted.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/object.hpp \
+ /usr/include/c++/11/vector /usr/include/c++/11/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++allocator.h \
+ /usr/include/c++/11/ext/new_allocator.h \
+ /usr/include/c++/11/bits/memoryfwd.h \
+ /usr/include/c++/11/bits/stl_construct.h \
+ /usr/include/c++/11/bits/stl_uninitialized.h \
+ /usr/include/c++/11/ext/alloc_traits.h \
+ /usr/include/c++/11/bits/alloc_traits.h \
+ /usr/include/c++/11/bits/stl_vector.h \
+ /usr/include/c++/11/bits/stl_bvector.h \
+ /usr/include/c++/11/bits/functional_hash.h \
+ /usr/include/c++/11/bits/hash_bytes.h \
+ /usr/include/c++/11/bits/vector.tcc \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/class_db.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/method_bind.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/binder_common.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/method_ptrcall.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/type_info.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/classes/global_constants_binds.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/variant/builtin_binds.hpp \
+ /usr/include/c++/11/string /usr/include/c++/11/bits/stringfwd.h \
+ /usr/include/c++/11/bits/char_traits.h \
+ /usr/include/c++/11/bits/postypes.h /usr/include/c++/11/cwchar \
+ /usr/include/wchar.h /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/c++/11/bits/localefwd.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++locale.h \
+ /usr/include/c++/11/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/11/iosfwd \
+ /usr/include/c++/11/cctype /usr/include/ctype.h \
+ /usr/include/c++/11/bits/ostream_insert.h \
+ /usr/include/c++/11/bits/cxxabi_forced.h \
+ /usr/include/c++/11/bits/stl_function.h \
+ /usr/include/c++/11/backward/binders.h \
+ /usr/include/c++/11/bits/basic_string.h \
+ /usr/include/c++/11/ext/atomicity.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/gthr.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_sched_param.h \
+ /usr/include/x86_64-linux-gnu/bits/cpu-set.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_tm.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_itimerspec.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/atomic_word.h \
+ /usr/include/x86_64-linux-gnu/sys/single_threaded.h \
+ /usr/include/c++/11/ext/string_conversions.h /usr/include/c++/11/cstdlib \
+ /usr/include/c++/11/cstdio /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/c++/11/cerrno /usr/include/errno.h \
+ /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/x86_64-linux-gnu/bits/types/error_t.h \
+ /usr/include/c++/11/bits/charconv.h \
+ /usr/include/c++/11/bits/basic_string.tcc /usr/include/c++/11/iostream \
+ /usr/include/c++/11/ostream /usr/include/c++/11/ios \
+ /usr/include/c++/11/exception /usr/include/c++/11/bits/exception_ptr.h \
+ /usr/include/c++/11/bits/cxxabi_init_exception.h \
+ /usr/include/c++/11/typeinfo /usr/include/c++/11/bits/nested_exception.h \
+ /usr/include/c++/11/bits/ios_base.h \
+ /usr/include/c++/11/bits/locale_classes.h \
+ /usr/include/c++/11/bits/locale_classes.tcc \
+ /usr/include/c++/11/system_error \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/error_constants.h \
+ /usr/include/c++/11/stdexcept /usr/include/c++/11/streambuf \
+ /usr/include/c++/11/bits/streambuf.tcc \
+ /usr/include/c++/11/bits/basic_ios.h \
+ /usr/include/c++/11/bits/locale_facets.h /usr/include/c++/11/cwctype \
+ /usr/include/wctype.h /usr/include/x86_64-linux-gnu/bits/wctype-wchar.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/ctype_base.h \
+ /usr/include/c++/11/bits/streambuf_iterator.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/ctype_inline.h \
+ /usr/include/c++/11/bits/locale_facets.tcc \
+ /usr/include/c++/11/bits/basic_ios.tcc \
+ /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
+ /usr/include/c++/11/bits/istream.tcc \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/gen/include/godot_cpp/classes/class_db_singleton.hpp \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/variant/callable_method_pointer.hpp \
+ /usr/include/c++/11/list /usr/include/c++/11/bits/stl_list.h \
+ /usr/include/c++/11/bits/allocated_ptr.h \
+ /usr/include/c++/11/ext/aligned_buffer.h \
+ /usr/include/c++/11/bits/list.tcc /usr/include/c++/11/mutex \
+ /usr/include/c++/11/tuple /usr/include/c++/11/bits/uses_allocator.h \
+ /usr/include/c++/11/bits/invoke.h /usr/include/c++/11/chrono \
+ /usr/include/c++/11/ratio /usr/include/c++/11/limits \
+ /usr/include/c++/11/ctime /usr/include/c++/11/bits/parse_numbers.h \
+ /usr/include/c++/11/bits/std_mutex.h \
+ /usr/include/c++/11/bits/unique_lock.h /usr/include/c++/11/set \
+ /usr/include/c++/11/bits/stl_tree.h /usr/include/c++/11/bits/stl_set.h \
+ /usr/include/c++/11/bits/stl_multiset.h \
+ /usr/include/c++/11/bits/erase_if.h /usr/include/c++/11/unordered_map \
+ /usr/include/c++/11/bits/hashtable.h \
+ /usr/include/c++/11/bits/hashtable_policy.h \
+ /usr/include/c++/11/bits/enable_special_members.h \
+ /usr/include/c++/11/bits/unordered_map.h \
+ /mnt/d/GitHub/maszyna-physics-server/godot-cpp/include/godot_cpp/core/engine_ptrcall.hpp
